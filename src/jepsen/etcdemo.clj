5949aa5a7f13ae47cbb32befa7118b2a8dec1d2a
Pedro Sland <pedro_sland@hotmail.com>
2018-03-22 14:49:02 +0000

    Refactor

--- src/jepsen/etcdemo.clj
+++ src/jepsen/etcdemo.clj
@@ -15,71 +15,8 @@
             [knossos.model :as model]
             [jepsen.control.util :as cu]
             [jepsen.checker.timeline :as timeline]
-            [jepsen.os.debian :as debian]))
-
-(def dir "/opt/etcd")
-(def binary "etcd")
-(def logfile (str dir "/etcd.log"))
-(def pidfile (str dir "/etcd.pid"))
-
-(defn node-url
-  "An HTTP url for connecting to a node on a particular port."
-  [node port]
-  (str "http://" (name node) ":" port))
-
-(defn peer-url
-  "The HTTP url for other peers to talk to a node."
-  [node]
-  (node-url node 2380))
-
-(defn client-url
-  "The HTTP url clients use to talk to a node."
-  [node]
-  (node-url node 2379))
-
-(defn initial-cluster
-  "Constructs an initial cluster string for a test, like
-  \"foo=foo:2380,bar=bar:2380,...\""
-  [test]
-  (->> (:nodes test)
-       (map (fn [node]
-              (str node "=" (peer-url node))))
-       (str/join ",")))
-
-(defn db
-  "Etcd at a particular version"
-  [version]
-  (reify db/DB
-         (setup! [db test node]
-                (let [url (str "https://storage.googleapis.com/etcd/" version
-                               "/etcd-" version "-linux-amd64.tar.gz")]
-                  (c/su
-                    (cu/install-archive! url dir)
-                    (cu/start-daemon!
-                     {:logfile logfile
-                      :pidfile pidfile
-                      :chdir dir}
-                     binary
-                     :--log-output                   :stderr
-                     :--name                         (name node)
-                     :--listen-peer-urls             (peer-url   node)
-                     :--listen-client-urls           (client-url node)
-                     :--advertise-client-urls        (client-url node)
-                     :--initial-cluster-state        :new
-                     :--initial-advertise-peer-urls  (peer-url node)
-                     :--initial-cluster              (initial-cluster test))))
-
-                    ; this should loop and wait for the db to be alive but we're lazy
-
-                    (Thread/sleep 10000))
-         (teardown! [db test node]
-                    (info node "tearing down etcd")
-                    (cu/stop-daemon! binary pidfile)
-                    (c/su (c/exec :rm :-rf dir)))
-
-         db/LogFiles
-         (log-files [db test node]
-                    [logfile])))
+            [jepsen.os.debian :as debian]
+            [jepsen.etcdemo.support :as s]))
 
 (defn r   [test process] {:type :invoke, :f :read, :value nil})
 (defn w   [test process] {:type :invoke, :f :write, :value (rand-int 5)})
@@ -99,7 +36,7 @@
   ; in open!, should always use a timeout.
   ; can use this if not provided by client lib: (util/timeout 5000 :default (can write stuff here))
   (open! [this test node]
-         (assoc this :conn (v/connect (client-url node)
+         (assoc this :conn (v/connect (s/client-url node)
                                       {:timeout 5000})))
 
   (setup! [this test])
@@ -139,7 +76,7 @@
       (merge tests/noop-test
            opts
            {:os debian/os
-            :db (db "v3.1.5")
+            :db (s/db "v3.1.5")
             :name (str "etcd q=" quorum)
             :quorum quorum
             :client (Client. nil)
